ca8c2e3f9183b6c9a9bdb05d7ca463609b6984b0
TutuStudy <47936817+TutuStudy@users.noreply.github.com>
2021-11-04 17:22:14 +0800

    Update Constants.java

--- ERPtest/src/main/java/com/yeelight/erp/crm/constants/Constants.java
+++ ERPtest/src/main/java/com/yeelight/erp/crm/constants/Constants.java
@@ -3,9 +3,5 @@ package com.yeelight.erp.crm.constants;
 public interface Constants {
 
 
-    String FORMID = "SAL_DELIVERYNOTICE";
-    String DBID = "****************";
-    String UID = "administrator";
-    String PWD = "***************;
-    Integer LANG = 2052;
+
 }
